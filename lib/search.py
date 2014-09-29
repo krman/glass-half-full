681f5f4b46a2fd1009b0d8417905b684479d5553
Kimberley Manning <kmanning@gmx.com>
2014-09-29 23:16:05 +1000

    small work on search

--- lib/search.py
+++ lib/search.py
@@ -2,21 +2,37 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from lib.visualise import *
-from lib.features import *
+from visualise import drawMatches, draw_matching_scene_keypoints
+from features import load_features
 
 
 def search_scene(scene, cups):
-    img1 = cv2.imread('images/small_cup_32.jpg',0)
+    img1 = cv2.imread('images/small_cup_up_1.jpg',0)
     img2 = cv2.imread(scene,0)
 
     sift = cv2.SIFT()
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
 
-    #bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-    #matches = bf.match(des1,des2)   
-    #matches = sorted(matches, key = lambda x:x.distance)
+    small_features = load_features('features/small_up.txt')
+    kp1 = []
+    for name in small_features:
+	kps,ds = small_features[name]
+	kp1 += kps
+	print len(kp1)
+	try:
+	    des1 = np.concatenate((des1, ds))
+	except NameError:
+	    des1 = ds
+
+    #kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+    print len(des1)
+    print len(des2)
+
+    """
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    matches = bf.match(des1,des2)   
+    matches = sorted(matches, key = lambda x:x.distance)
+    """
 
     MIN_MATCH_COUNT = 10
 
@@ -27,13 +43,12 @@ def search_scene(scene, cups):
     flann = cv2.FlannBasedMatcher(index_params, search_params)
 
     matches = flann.knnMatch(des1,des2,k=2)
-    I1 = img1
     I2 = img2
 
     # store all the good matches as per Lowe's ratio test.
     good = []
     for m,n in matches:
-	if m.distance < 0.8*n.distance:
+	if m.distance < 0.7*n.distance:
 	    good.append(m)
     #good = [m for m,n in matches]
 
@@ -55,4 +70,7 @@ def search_scene(scene, cups):
 	matchesMask = None
 
     matches = [m for i,m in enumerate(good) if 1 or matchesMask[i]]
-    img3 = drawMatches(img1,kp1,img2,kp2,good)
+    #img3 = overlay_scene_keypoints(img2, kp2, matches)
+    img3 = cv2.drawKeypoints(img2, kp2)
+    cv2.imwrite("hello.jpg", img3)
+    #img3 = drawMatches(img1,kp1,img2,kp2,good)
