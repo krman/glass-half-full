9dc9954c86a5f9ce9921aea23d99ef5b35ae808a
Kimberley Manning <kmanning@gmx.com>
2014-10-02 19:23:14 +1000

    remove single-camera calibration

--- lib/calibrate.py
+++ lib/calibrate.py
@@ -45,45 +45,6 @@ def get_fundamental_matrix(left_image, right_image):
     return F
 
 
-def calibrate_camera(filenames):
-    # termination criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6*8,3), np.float32)
-    objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
-
-    # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
-
-    images = glob.glob(filenames)
-
-    for fname in sorted(images):
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
-
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            objpoints.append(objp)
-
-            cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-            imgpoints.append(corners)
-
-            # Draw and display the corners
-            #cv2.drawChessboardCorners(img, (8,6), corners, ret)
-
-        yield False,fname,ret,corners
-
-
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints,
-            gray.shape[::-1],None,None)
-    yield True,mtx,None,None
-
-
 def calibrate_stereo(left_filenames, right_filenames):
     left_images = sorted(glob.glob(left_filenames))
     right_images = sorted(glob.glob(right_filenames))
